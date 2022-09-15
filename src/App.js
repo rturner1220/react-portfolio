9669def90da89625ebb35034bef84de64f2a6e75
Rosa Turner <rmturner1220@gmail.com>
2022-09-15 19:51:40 -0400

    contact form done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './component/Nav';
 import About from './component/About';
+import Portfolio from './component/Portfolio';
 
 function App() {
+  function App() {
+    const [pages] = useState([
+      { name: "about me" },
+      { name: "portfolio" },
+      { name: "contact" },
+      { name: "resume" }
+    ]);
 
+    const [currentPage, setCurrentPage] = useState(pages[0]);
 
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
-}
+    return (
+      <div>
 
-export default App;
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+
+        <main>
+          <Page currentPage={currentPage}></Page>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+
+  export default App;
--- src/component/About/index.js
+++ src/component/About/index.js
@@ -7,7 +7,12 @@ function About() {
         <section className="my-5">
             <h1 id="about">About me
             </h1>
-            <img src={profileImage} className="my-2" style={{ width: "15%" }} alt="cover" />
+            <img src={profileImage} className="my-2" style={{ width: "15%" }} alt="" />
+            <div className="my-2">
+                <p>
+                    Front-end web developer leveraging background in psychology to provide unique perspectives on how end-users interact with websites and software platforms. Earned a certificate in Full Stack Web Development from the University Coding Boot Camp. Innovative problem-solver who is passionate about developing apps with a focus on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution.
+                </p>
+            </div>
         </section>
     )
 }
