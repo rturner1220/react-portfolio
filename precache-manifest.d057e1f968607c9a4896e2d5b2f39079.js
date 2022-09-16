e2b114213f12722a6b60659369138003f015982f
Rosa Turner <rmturner1220@gmail.com>
2022-09-16 09:58:03 -0400

    Updates

---
+++ precache-manifest.d057e1f968607c9a4896e2d5b2f39079.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3099a37e9c04f7a6b0",
+    "url": "/rturner1220/React-Portfolio/static/css/main.9860899a.chunk.css"
+  },
+  {
+    "revision": "3e3099a37e9c04f7a6b0",
+    "url": "/rturner1220/React-Portfolio/static/js/main.3e3099a3.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/rturner1220/React-Portfolio/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "b85c813065228f218961",
+    "url": "/rturner1220/React-Portfolio/static/js/2.b85c8130.chunk.js"
+  },
+  {
+    "revision": "63ab4a0e82ffadb587bb",
+    "url": "/rturner1220/React-Portfolio/static/js/runtime~main.63ab4a0e.js"
+  },
+  {
+    "revision": "d4beac3a2bf5d2367dbb98bc4f84d5cc",
+    "url": "/rturner1220/React-Portfolio/static/media/profile pic.d4beac3a.jpg"
+  },
+  {
+    "revision": "f3e9af40031ab5a276252150f43bbfa1",
+    "url": "/rturner1220/React-Portfolio/static/media/app01.f3e9af40.png"
+  },
+  {
+    "revision": "96b150884fc77a5e8bc4657ece540313",
+    "url": "/rturner1220/React-Portfolio/static/media/app02.96b15088.png"
+  },
+  {
+    "revision": "f89fa9290bb87b6187e731dd2501f07c",
+    "url": "/rturner1220/React-Portfolio/static/media/app03.f89fa929.png"
+  },
+  {
+    "revision": "774a9c7467be52082e48c7cad6457ebc",
+    "url": "/rturner1220/React-Portfolio/static/media/app04.774a9c74.png"
+  },
+  {
+    "revision": "ff2e99e53d3d0b483b7b7d614d2f342e",
+    "url": "/rturner1220/React-Portfolio/static/media/app05.ff2e99e5.png"
+  },
+  {
+    "revision": "35f894f1ff6b3cbbb15cb2ff1cfd57a8",
+    "url": "/rturner1220/React-Portfolio/static/media/app06.35f894f1.png"
+  },
+  {
+    "revision": "d2fa5d00d302f59e9a263d3150d294a0",
+    "url": "/rturner1220/React-Portfolio/static/media/header.d2fa5d00.jpg"
+  },
+  {
+    "revision": "b3782ca3d9338b2a15c1834ac90b2676",
+    "url": "/rturner1220/React-Portfolio/index.html"
+  }
+];
