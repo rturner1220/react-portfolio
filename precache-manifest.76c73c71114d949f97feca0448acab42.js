fdb6c48f844d5603a79133337d8873efc94d198f
Rosa Turner <rmturner1220@gmail.com>
2022-09-20 11:00:15 -0400

    Updates

---
+++ precache-manifest.76c73c71114d949f97feca0448acab42.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7409f8a3774b0476a986",
+    "url": "/react-portfolio/static/css/main.8b7d2da0.chunk.css"
+  },
+  {
+    "revision": "7409f8a3774b0476a986",
+    "url": "/react-portfolio/static/js/main.7409f8a3.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/react-portfolio/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "be05767538d412b37818",
+    "url": "/react-portfolio/static/js/2.be057675.chunk.js"
+  },
+  {
+    "revision": "9338963966fbdc171994",
+    "url": "/react-portfolio/static/js/runtime~main.93389639.js"
+  },
+  {
+    "revision": "d4beac3a2bf5d2367dbb98bc4f84d5cc",
+    "url": "/react-portfolio/static/media/profile pic.d4beac3a.jpg"
+  },
+  {
+    "revision": "f3e9af40031ab5a276252150f43bbfa1",
+    "url": "/react-portfolio/static/media/app01.f3e9af40.png"
+  },
+  {
+    "revision": "96b150884fc77a5e8bc4657ece540313",
+    "url": "/react-portfolio/static/media/app02.96b15088.png"
+  },
+  {
+    "revision": "f89fa9290bb87b6187e731dd2501f07c",
+    "url": "/react-portfolio/static/media/app03.f89fa929.png"
+  },
+  {
+    "revision": "774a9c7467be52082e48c7cad6457ebc",
+    "url": "/react-portfolio/static/media/app04.774a9c74.png"
+  },
+  {
+    "revision": "ff2e99e53d3d0b483b7b7d614d2f342e",
+    "url": "/react-portfolio/static/media/app05.ff2e99e5.png"
+  },
+  {
+    "revision": "35f894f1ff6b3cbbb15cb2ff1cfd57a8",
+    "url": "/react-portfolio/static/media/app06.35f894f1.png"
+  },
+  {
+    "revision": "42d0fc3be82f5d1f671ac2fd2475f8bf",
+    "url": "/react-portfolio/static/media/Rosa_Turner_Resume.42d0fc3b.pdf"
+  },
+  {
+    "revision": "d2fa5d00d302f59e9a263d3150d294a0",
+    "url": "/react-portfolio/static/media/header.d2fa5d00.jpg"
+  },
+  {
+    "revision": "1eace345da7077d41e639ad40f7e40db",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rturner1220/React-Portfolio/precache-manifest.96905416688d3af9e2d777c99f18ee43.js"
+  "/react-portfolio/precache-manifest.76c73c71114d949f97feca0448acab42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rturner1220/React-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
